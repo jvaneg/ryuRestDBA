67b9b0a1444e942c82877f622acdd44abbe7027e
joel <joel.vanegmond@ucalgary.ca>
2019-03-04 23:56:48 -0700

    fixed bug in get_minimum_rate()

--- meter.py
+++ meter.py
@@ -1,3 +1,6 @@
+KILOBIT_TO_MEGABIT_FACTOR = 1000
+
+
 # This class mostly exists as a container because I wanted to avoid using pure dicts for everything
 class Meter:
 
@@ -13,5 +16,6 @@ class Meter:
     def get_id(self):
         return int(self.properties["meter_id"])
 
+    # returns the min rate in mbps
     def get_min_rate(self):
-        return int(self.properties["bands"]["rate"])
+        return int(self.properties["bands"][0]["rate"])/KILOBIT_TO_MEGABIT_FACTOR
