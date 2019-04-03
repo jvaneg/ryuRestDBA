e7e79259fba827f0c8dcf36d2b70922e572f0ac0
Joel <joel.vanegmond@ucalgary.ca>
2019-04-03 11:37:33 -0600

    fixed meter rate 0 bug

--- meter.py
+++ meter.py
@@ -26,6 +26,8 @@ class Meter:
 
     def set_rate(self, new_rate_mbps):
         print(new_rate_mbps)
+        if(new_rate_mbps == 0):
+            new_rate_mbps = 1
         self.properties["bands"][0]["rate"] = int(new_rate_mbps)*MEGABIT_TO_KILOBIT_FACTOR
 
     def to_dict(self):
