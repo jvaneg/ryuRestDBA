ec332af037672700176b7e5eb970dde2c61aa8f7
Joel <joel.vanegmond@ucalgary.ca>
2019-04-04 21:23:35 -0600

    Improved plot tool restructured folder structure Added more comments to controller

--- controller.py
+++ controller.py
@@ -4,8 +4,8 @@ import time
 from pathlib import Path
 
 # my module imports
-import dbaAlgorithms
-import switchTools
+from backend import dbaAlgorithms
+from backend import switchTools
 
 # 3rd party imports
 import toml
@@ -32,47 +32,67 @@ def main(args):
     tier2_switch = switch_list[TIER2_SWITCH]
     tier3_switch = switch_list[TIER3_SWITCH]
 
-    # get initial flow bytes usage
+    # get initial flow bytes usage TODO: remove this
     flow_bytes, timestamp = switchTools.get_flow_bytes(tier1_switch[0])
 
     print(timestamp)    # TODO: remove debug
     print(flow_bytes)
 
-    flow_list = tier1_switch[2]
-
+    t1_flow_list = tier1_switch[2]
     link_flows(tier1_switch, tier2_switch)
 
-    print("Link capacity: {}".format(link_capacity))
+    print("Link capacity: {}".format(link_capacity))  # TODO: remove debug
 
-    for flow_id, flow in flow_list.items():
+    # TODO: remove this debug stuff
+    for flow_id, flow in t1_flow_list.items():
         print("Flow: {} - Meter: {} - Min rate: {} Mbps".format(flow_id, flow.get_meter(), flow.get_minimum_rate()))
 
     print("--- Flow bandwidth ---")
 
+    # open log file
     if(args.log is not None):
-        # open log file
         log_file = open(args.log, "w+")
 
     try:
+        # loop forever:
+        #   poll tier 1 flows
+        #   calculate bandwidth usage (target bandwidth)
+        #   calculate tier 2 meter maximums (dependent on algorithm)
+        #   modify tier 2 meters structures
+        #   install changes on switch
+        #   if logging:
+        #       poll tier 3 flows
+        #       log data to file
         while(True):
-            for flow_id, flow in flow_list.items():
+            # poll t1 switch flows for flow information
+            # flow_bytes, timestamp = switchTools.get_flow_bytes(tier1_switch[0])
+
+            # calculate demand bandwidth for each flow
+            for flow_id, flow in t1_flow_list.items():
                 flow.update_demand_bw(flow_bytes[flow_id], timestamp)
 
-            excess_bandwidth = calc_excess_bandwidth(flow_list, link_capacity)
+            # calculate excess bandwidth TODO: may not need this with different alg implementation
+            excess_bandwidth = calc_excess_bandwidth(t1_flow_list, link_capacity)
 
-            dbaAlgorithms.allocate_egalitarian(flow_list, excess_bandwidth)
+            # calculate bandwidth allocation for each flow TODO: value in config to determine which alg
+            dbaAlgorithms.allocate_egalitarian(t1_flow_list, excess_bandwidth)
+
+            # install allocated bandwidth changes on t2 switch meters
             switchTools.update_meters(tier2_switch[0], tier2_switch[2])
 
+            # logging data
             if(args.log is not None):
+                # TODO: poll t3 switch for flow data
+
                 flow_demand_display = "Demand:\t"
                 demand_csv_string = ""
-                for flow_id, flow in flow_list.items():
+                for flow_id, flow in t1_flow_list.items():
                     flow_demand_display += "{} - {}\t  ".format(flow_id, flow.get_demand_bw())
                     demand_csv_string += "{},".format(flow.get_demand_bw())
 
                 flow_allocated_display = "Allocated:\t"
                 allocated_csv_string = ""
-                for flow_id, flow in flow_list.items():
+                for flow_id, flow in t1_flow_list.items():
                     flow_allocated_display += "{} - {}\t  ".format(flow_id, flow.get_allocated_bw())
                     allocated_csv_string += "{},".format(flow.get_allocated_bw())
 
@@ -85,22 +105,24 @@ def main(args):
                 # log to file
                 log_file.write("{},{}{}\n".format(excess_bandwidth, demand_csv_string, allocated_csv_string))
 
-            # read again 
-            flow_bytes, timestamp = switchTools.get_flow_bytes(tier1_switch[0])
+            # wait
             time.sleep(1)
-    except (KeyboardInterrupt, SystemExit, Exception) as e:
+
+            # read again
+            flow_bytes, timestamp = switchTools.get_flow_bytes(tier1_switch[0])
+    except (KeyboardInterrupt, SystemExit):
+        pass
+    except Exception as e:
         print(e)
+    finally:
         print("Cleaning up...")
+
+        # close log file
         if(args.log is not None):
             log_file.close()
-        switchTools.clean_switches(args.config_file)
 
-    # loop forever
-    #   poll flows
-    #   calculate bandwidth usage (target bandwidth)
-    #   calculate tier 2 meter maximums (dependent on algorithm)
-    #   modify tier 2 meters structures
-    #   install changes on switch
+        # clean switch
+        switchTools.clean_switches(args.config_file)
 
 
 # this function will let you return negative numbers
@@ -123,6 +145,8 @@ def calc_excess_bandwidth(flow_list, link_capacity):
     return excess_bandwidth
 
 
+# links the flows of the t1 switch with the corresponding flows of the t2 switch
+# this is to make it easier to set the meter maximums
 def link_flows(tier1_switch_tuple, tier2_switch_tuple):
     t1_flows = tier1_switch_tuple[2]
     t2_flows = tier2_switch_tuple[2]
---
+++ tools/plotTool.py
@@ -0,0 +1,334 @@
+import argparse
+from pathlib import Path
+import csv
+
+import plotly.graph_objs as go
+import plotly.io as pio
+
+
+def main(args):
+    # check if target file exists and load it
+    if(not Path(args.target_csv).is_file()):
+        print("Error loading target csv file")
+        exit(-1)
+
+    if(args.title is not None):
+        title = args.title
+    else:
+        title = "Bandwidth over time"
+
+    csv_data = {
+        "excess": [],
+        "demand_h1h3": [],
+        "demand_h2h3": [],
+        "allocation_h1h3": [],
+        "allocation_h2h3": [],
+        "high_h1h3": [],
+        "high_h2h3": [],
+        "med_h1h3": [],
+        "med_h2h3": [],
+        "low_h1h3": [],
+        "low_h2h3": [],
+        "minrate_h1": [],
+        "minrate_h2": [],
+        "x_axis": [],
+    }
+
+    with Path(args.target_csv).open() as csv_file:
+        csv_format = csv.Sniffer().sniff(csv_file.read())
+        csv_file.seek(0)
+        csv_reader = csv.reader(csv_file, csv_format)
+        for row in csv_reader:
+            csv_data["excess"].append(float(row[0]))
+
+            csv_data["demand_h1h3"].append(float(row[1]))
+            csv_data["demand_h2h3"].append(float(row[2]))
+
+            csv_data["allocation_h1h3"].append(float(row[3]))
+            csv_data["allocation_h2h3"].append(float(row[4]))
+
+            csv_data["high_h1h3"].append(float(row[5]))
+            csv_data["high_h2h3"].append(float(row[6]))
+
+            csv_data["med_h1h3"].append(float(row[7]))
+            csv_data["med_h2h3"].append(float(row[8]))
+
+            csv_data["low_h1h3"].append(float(row[9]))
+            csv_data["low_h2h3"].append(float(row[10]))
+
+            csv_data["minrate_h1"].append(float(row[11]))
+            csv_data["minrate_h2"].append(float(row[12]))
+
+        csv_data["x_axis"] = list(range(len(csv_data["excess"])))
+
+    if(args.type is not None):
+        if(args.type == "h1"):
+            data = h1_graph(csv_data)
+        elif(args.type == "h2"):
+            data = h2_graph(csv_data)
+        else:
+            data = full_graph(csv_data)
+    else:
+        data = full_graph(csv_data)
+
+    # Edit the layout
+    layout = dict(
+        title=title,
+        xaxis=dict(title="Time (seconds)"),
+        yaxis=dict(title="Bandwidth (Mbps)"),
+        )
+
+    fig = dict(data=data, layout=layout)
+
+    if(args.output_name is None):
+        output_file = "{}_{}.png".format(Path(args.target_csv).stem, args.type)
+    else:
+        output_file = args.output_name
+
+    pio.write_image(fig, output_file, width=FIG_WIDTH, height=FIG_HEIGHT, scale=1)
+    print("Graph written to {}".format(output_file))
+
+
+# full detailed graph
+# shows:
+# H1 demand
+# H2 demand
+# H1 alloc
+# H2 alloc
+def full_graph(csv_data):
+    # Create and style traces
+
+    # excess_trace = go.Scatter(
+    #     x = csv_data["x_axis"],
+    #     y = csv_data["excess"],
+    #     name = "Excess Bandwidth",
+    #     line = dict(
+    #         color = ("#B18FCF"),
+    #         width = 4,
+    #         dash = "dashdot")
+    # )
+    h1_demand_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["demand_h1h3"],
+        name="Demand H1->H3",
+        line=dict(
+            color=WARM_1,
+            width=TWO_LINE_SIZE,
+            ),
+    )
+    h1_alloc_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["allocation_h1h3"],
+        name="Allocation H1->H3",
+        line=dict(
+            color=WARM_2,
+            width=TWO_LINE_SIZE,
+            dash="dot",  # dash options include 'dash', 'dot', and 'dashdot'
+            ),
+    )
+    h2_demand_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["demand_h2h3"],
+        name="Demand H2->H3",
+        line=dict(
+            color=COLD_1,
+            width=TWO_LINE_SIZE,
+            ),
+    )
+    h2_alloc_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["allocation_h2h3"],
+        name="Allocation H2->H3",
+        line=dict(
+            color=COLD_2,
+            width=TWO_LINE_SIZE,
+            dash="dot",
+            ),
+    )
+
+    data = [h1_demand_trace, h2_demand_trace, h1_alloc_trace, h2_alloc_trace]
+    return data
+
+
+# h1 graph - warm colours
+# shows:
+# H1 demand
+# H1 allocation
+# H1 high priority
+# H1 med priority
+# H1 low priority (dropped)
+def h1_graph(csv_data):
+    # Create and style traces
+
+    demand_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["demand_h1h3"],
+        name="Demand H1->H3 Bandwidth",
+        line=dict(
+            color=WARM_1,
+            width=ONE_LINE_SIZE,
+            ),
+    )
+    alloc_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["allocation_h1h3"],
+        name="Allocation H1->H3",
+        line=dict(
+            color=WARM_2,
+            width=ONE_LINE_SIZE,
+            dash="dot",
+            ),
+    )
+    high_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["high_h1h3"],
+        name="High Priority H1->H3",
+        line=dict(
+            color=WARM_3,
+            width=ONE_LINE_SIZE,
+            dash="dash",
+            ),
+    )
+    med_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["med_h1h3"],
+        name="Medium Priority H1->H3",
+        line=dict(
+            color=WARM_4,
+            width=ONE_LINE_SIZE,
+            dash="dashdot",
+            ),
+    )
+    low_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["low_h1h3"],
+        name="Low Priority H1->H3",
+        line=dict(
+            color=WARM_5,
+            width=ONE_LINE_SIZE,
+            ),
+    )
+    minrate_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["minrate_h1"],
+        name="Minimum Rate H1",
+        line=dict(
+            color=BLACK,
+            width=MIN_RATE_LINE_SIZE,
+            dash="dash",
+            ),
+    )
+    data = [minrate_trace, demand_trace, alloc_trace, high_trace, med_trace, low_trace]
+    return data
+
+
+# h2 graph - cold colours
+# shows:
+# H2 demand
+# H2 allocation
+# H2 high priority
+# H2 med priority
+# H2 low priority (dropped)
+def h2_graph(csv_data):
+    # Create and style traces
+
+    demand_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["demand_h2h3"],
+        name="Demand H2->H3 Bandwidth",
+        line=dict(
+            color=COLD_1,
+            width=ONE_LINE_SIZE,
+            ),
+    )
+    alloc_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["allocation_h2h3"],
+        name="Allocation H2->H3",
+        line=dict(
+            color=COLD_2,
+            width=ONE_LINE_SIZE,
+            dash="dot",
+            ),
+    )
+    high_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["high_h2h3"],
+        name="High Priority H2->H3",
+        line=dict(
+            color=COLD_3,
+            width=ONE_LINE_SIZE,
+            dash="dashdot",
+            ),
+    )
+    med_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["med_h2h3"],
+        name="Medium Priority H2->H3",
+        line=dict(
+            color=COLD_4,
+            width=ONE_LINE_SIZE,
+            dash="dash",
+            ),
+    )
+    low_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["low_h2h3"],
+        name="Low Priority H2->H3",
+        line=dict(
+            color=COLD_5,
+            width=ONE_LINE_SIZE,
+            ),
+    )
+    minrate_trace = go.Scatter(
+        x=csv_data["x_axis"],
+        y=csv_data["minrate_h2"],
+        name="Minimum Rate H2",
+        line=dict(
+            color=BLACK,
+            width=MIN_RATE_LINE_SIZE,
+            dash="dash",
+            ),
+    )
+    data = [minrate_trace, demand_trace, alloc_trace, high_trace, med_trace, low_trace]
+    return data
+
+
+# warm palette
+WARM_1 = "#900C3F"
+WARM_2 = "#C70039"
+WARM_3 = "#FF5733"
+WARM_4 = "#CC7178"
+WARM_5 = "#FFC300"
+
+# cold palette
+COLD_1 = "#113F67"
+COLD_2 = "#34699A"
+COLD_3 = "#408AB4"
+COLD_4 = "#65C6C4"
+COLD_5 = "#ACDEAA"
+
+# other colours
+BLACK = "#000000"
+
+# line sizes
+ONE_LINE_SIZE = 5
+TWO_LINE_SIZE = 5
+MIN_RATE_LINE_SIZE = 3
+
+# graph size
+FIG_WIDTH = 1200
+FIG_HEIGHT = 700
+
+# main
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target_csv", help="the csv file to be parsed")
+    parser.add_argument("-o", "--output_name", metavar="output_name", help="filename of graph to output")
+    parser.add_argument("-gt", "--title", metavar="graph_title", help="graph title")
+    parser.add_argument("-t", "--type", metavar="graph_type",
+                        default="both",
+                        choices=["h1", "h2", "both"],
+                        help="type of graph")
+    args = parser.parse_args()
+    main(args)
