87f20c7632a4d308b7b419493f55fc605700819a
joel <joel.vanegmond@ucalgary.ca>
2019-04-08 11:02:04 -0600

    bad brain

--- backend/dbaAlgorithms.py
+++ backend/dbaAlgorithms.py
@@ -161,7 +161,7 @@ def allocate_hybrid(flow_list, excess_bandwidth, max_fraction):
                 print("{} - path 3  fx {} fm {} xs {} rx {}".format(flow.get_id, flow_extra, flow_maximum, flow_excess_share, remaining_excess))
                 active_flows.remove(flow)
             else:
-                flow.allocated_bw += flow_excess_share
+                flow.allocated_bw += flow_excess_share + flow_maximum
                 remaining_excess -= flow_excess_share
                 print("{} - path 4  fx {} fm {} xs {} rx {}".format(flow.get_id, flow_extra, flow_maximum, flow_excess_share, remaining_excess))
 
